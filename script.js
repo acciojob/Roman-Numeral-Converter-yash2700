60063855ab0c0ee9e11276b99905c94733fc0b62
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-05-29 20:06:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  // Define arrays for Roman numerals and their corresponding values
+  const romanNumerals = [
+    { value: 100000, numeral: 'C' },
+    { value: 90000, numeral: 'XC' },
+    { value: 50000, numeral: 'L' },
+    { value: 40000, numeral: 'XL' },
+    { value: 10000, numeral: 'X' },
+    { value: 9000, numeral: 'IX' },
+    { value: 5000, numeral: 'V' },
+    { value: 4000, numeral: 'IV' },
+    { value: 1000, numeral: 'M' },
+    { value: 900, numeral: 'CM' },
+    { value: 500, numeral: 'D' },
+    { value: 400, numeral: 'CD' },
+    { value: 100, numeral: 'C' },
+    { value: 90, numeral: 'XC' },
+    { value: 50, numeral: 'L' },
+    { value: 40, numeral: 'XL' },
+    { value: 10, numeral: 'X' },
+    { value: 9, numeral: 'IX' },
+    { value: 5, numeral: 'V' },
+    { value: 4, numeral: 'IV' },
+    { value: 1, numeral: 'I' }
+  ];
 
-  //your code here
+  let result = '';
+  for (let i = 0; i < romanNumerals.length; i++) {
+    while (num >= romanNumerals[i].value) {
+      result += romanNumerals[i].numeral;
+      num -= romanNumerals[i].value;
+    }
+  }
 
+  return result;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
